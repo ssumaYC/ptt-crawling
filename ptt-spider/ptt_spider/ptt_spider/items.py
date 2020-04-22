1b0eb4cc8243a438fd547a27751a7d700ef7d4ae
simaYC <s226012004@hotmail.com>
2020-04-22 20:43:17 +0800

    define clean and transform process in ItemLoader

--- ptt-spider/ptt_spider/ptt_spider/items.py
+++ ptt-spider/ptt_spider/ptt_spider/items.py
@@ -7,15 +7,10 @@
 
 import scrapy
 from scrapy.loader import ItemLoader
-from scrapy.loader.processors import TakeFirst, MapCompose, Compose
+from scrapy.loader.processors import TakeFirst, MapCompose, Identity
 from w3lib.html import remove_tags
 import re
 
-
-class PttSpiderItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
 
 class PttArticleItem(scrapy.Item):
     authorId = scrapy.Field()
@@ -28,14 +23,24 @@ class PttArticleItem(scrapy.Item):
     updateTime = scrapy.Field()
     comments = scrapy.Field()
 
-def clean_text(text):
-    t = remove_tags(text)
-    t = t.replace("\xa0", "")
-    t = t.strip()
-    return t
+
+def extract_content(text):
+    # If the article is a reply of other article the content of the
+    # original article will be warpped in <span> so we need to
+
+    # first remove all the div tag and the text been wrapped
+    regex = re.compile(r"<div [^>]*>[\w\W]*?</div>")
+    text = regex.sub("", text)
+    # then remove the span tag only remain the text been wrapped
+    text = remove_tags(text)
+    return text
+
 
 class PttArticleItemLoader(ItemLoader):
-
+    # the clean and transform process of certain fields
     default_output_processor = TakeFirst()
-    authorId_in = Compose(lambda l: l[0].split(" ")[0])
-    authorName_in = Compose(lambda l: l[0].split(" ")[1][1:-1])
+    authorId_in = MapCompose(lambda text: text.split(" ")[0])
+    authorName_in = MapCompose(lambda text: text.split(" ")[1][1:-1])
+    publishedTime_in = MapCompose(lambda dt: int(dt.timestamp() * 1000))
+    content_in = MapCompose(extract_content)
+    comments_out = Identity()
